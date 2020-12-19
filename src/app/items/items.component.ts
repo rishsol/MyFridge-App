de3ebb3047e51bd10d5943df1c537da6af2783bf
rishsol <rishabsolanki09@gmail.com>
2020-12-19 17:02:54 -0500

    working with ngmodel

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -4,4 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-items',
   templateUrl: './items.component.html'
 })
-export class ItemsComponent { }
+export class ItemsComponent {
+  addedItem = '';
+  addedDate = '';
+
+  newItem = '';
+  newDate = '';
+
+  addToFridge() {
+    this.newItem = this.addedItem;
+    this.newDate = this.addedDate;
+  }
+  addItem() {
+    document.append("<input type='text' [(ngModel)]='addedItem'><input type='date' [(ngModel)]='addedDate'><button> Delete </button>");
+  }
+}
